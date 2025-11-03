e5af9d88e4ce9774e8df1ad5d51588c1bb619659
Julian Bastidas <julian.bastidas@roda.xyz>
2025-11-03 02:11:14 -0500

    style: ajustar la indentación de las importaciones en roles.guard.ts

--- src/roles/roles.guard.ts
+++ src/roles/roles.guard.ts
@@ -1,8 +1,8 @@
 import {
-    CanActivate,
-    ExecutionContext,
-    Injectable,
-    UnauthorizedException,
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { RoleEnum } from './roles.enum';
